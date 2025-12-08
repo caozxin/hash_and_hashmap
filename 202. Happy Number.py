271030bc4be5ca0149eb4deff89c9e9ff4b6a9b3
Zhixin Cao <111973939+caozxin@users.noreply.github.com>
2025-12-08 06:50:50 -0500

    Update 202. Happy Number.py

--- 202. Happy Number.py
+++ 202. Happy Number.py
@@ -1,1 +1,34 @@
 
+class Solution:
+    def isHappy(self, n: int) -> bool:
+
+        def refine_num (num: int, visited: list, digit_squared:list) -> bool:
+            if num == 1:
+                return True
+
+            str_num = str(num)
+            #base exit:
+            
+            curr_sum = 0
+            for each_dgt in str_num:
+                each_dgt = int(each_dgt)
+                curr_sum += digit_squared[each_dgt]
+            print("curr_sum", curr_sum)
+            if curr_sum == 1:
+                print("true")
+                return True 
+            if curr_sum in visited:
+                return False
+            visited.append(curr_sum)
+            refine_num(curr_sum, visited, digit_squared)
+            
+            print(curr_sum, "here")
+        
+        digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        digit_squared = {}
+        for each_dgt in digits:
+            digit_squared[each_dgt] = each_dgt ** 2
+
+
+        visited = []
+        return refine_num(n, visited, digit_squared)
